f452d5ddb1801d6e27781a70e91c0c0b2ba80973
Adèle Delamarche <adele.delamarche@gmail.com>
2015-01-16 18:30:39 +0100

    Add some CSS for the example

--- sample/sample.jsx
+++ sample/sample.jsx
@@ -12,7 +12,7 @@ var App = React.createClass({
     return (
       <div>
         <ConfirmationBoxView actionName={'Delete my account'}
-                             actionMessage={'Be careful, the account deleteion is an irreversible action.'}
+                             actionMessage={'Be careful, the account deleteion is an irreversible action : you will not be able to sign-in on the site. If you encounter a problem of use, please contact us.'}
                              inputMarkup={'<input id="id_password" name="password" type="password">'}
                              warningMessage={'To permanently delete your Ulule account, please enter your password and click on the red button'}
                              validationName={'Validate the definitive deletion of my account'}
